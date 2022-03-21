2ffde53cbf7653caaff3a0e9fface8556ca2b4f0
Yiyun Qin <yiyun.qin@mths.ca>
2022-03-21 18:45:30 +0000

    number guess game

--- constants.py
+++ constants.py
@@ -1,6 +1,4 @@
-# constant definition
+# constant about the program number_guess
 # this module is called "constants" and has following lines of code:
 
-LABOR = 0.75
-RENT = 1.00
-MATERIAL_PER_INCH = 0.50
+ANSWER = 6
